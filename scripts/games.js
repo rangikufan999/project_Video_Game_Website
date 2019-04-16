56970a916cc1f4b570165dcdd37745785da8ffa3
Brandyn Porter <rangikufan999@gmail.com>
2019-04-16 12:17:15 -0500

    Initialization of Video Game Website Repository
    Initialization of project_Video_Game_Website

---
+++ scripts/games.js
@@ -0,0 +1,21 @@
+function showElement(element){
+	var whichElement = element.value;
+
+	switch(whichElement){
+		case "home":
+			$("#home").slideToggle("1000", "swing");
+		break;
+
+		case "about":
+		
+		break;
+
+		case "contact":
+
+		break;
+
+		case "join":
+
+		break;
+	}
+}
