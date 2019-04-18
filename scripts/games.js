bb8c9eeee5ab78228c49207dba50500f61da6a70
Brandyn Porter <rangikufan999@gmail.com>
2019-04-18 11:36:17 -0500

    Filling out Content on Site
    Made changes to increase the content on the site. Added about.html, contact.html, and join.html. Added some images for background and link images.

--- scripts/games.js
+++ scripts/games.js
@@ -1,21 +1,21 @@
 function showElement(element){
-	var whichElement = element.value;
-
+	var whichElement = element.getAttribute("value");
+	
 	switch(whichElement){
 		case "home":
 			$("#home").slideToggle("1000", "swing");
 		break;
 
 		case "about":
-		
+			$("#about").slideToggle("1000", "swing");
 		break;
 
 		case "contact":
-
+			$("#contact").slideToggle("1000", "swing");
 		break;
 
 		case "join":
-
+			$("#join").slideToggle("1000", "swing");
 		break;
 	}
 }
